45d53e2fd05d55f3a823811464d6991f2ca9eba7
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-06-06 20:26:45 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+
+  post_count = rand(1..5)
+  post_count.times do
+    post = user.posts.create!(title: Faker::Lorem.sentence(word_count: 3),
+                              body: Faker::Lorem.paragraph(sentence_count: 3))
+    post.comments.create!(user: user, body: Faker::Lorem.sentence(word_count: 3))
+  end
+end
