94282654d6194c37c1ee53022cf80916b59775d3
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-06-06 21:03:21 +0300

    Update types

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,9 +4,15 @@ module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer, null: false
-    field :title, String
-    field :body, String
+    field :title, String, null: true
+    field :body, String, null: true
+    field :comments, [Types::CommentType], null: true
+    field :comments_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def comments_count
+      object.comments.size
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,10 +2,16 @@
 
 module Types
   class UserType < Types::BaseObject
-    field :id, ID, null: false
-    field :email, String
-    field :name, String
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :id, ID, null: false, description: 'User id'
+    field :email, String, null: true, description: 'User email'
+    field :name, String, null: true, description: 'User name'
+    field :posts, [Types::PostType], null: true, description: 'User posts'
+    field :posts_count, Integer, null: true, description: 'User posts count'
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'Creation timestamp'
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'Updation timestamp'
+
+    def posts_count
+      object.posts.size
+    end
   end
 end
