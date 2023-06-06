cda6ffc0e69f34312b9438c02021b9f5837c7b6d
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-06-06 21:40:55 +0300

    Change some type structure

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -4,8 +4,10 @@ module Types
   class CommentType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false
     field :post_id, Integer, null: false
-    field :body, String
+    field :post, Types::PostType, null: false
+    field :body, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,6 +4,7 @@ module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false
     field :title, String, null: true
     field :body, String, null: true
     field :comments, [Types::CommentType], null: true
