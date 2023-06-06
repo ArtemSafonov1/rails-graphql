37a3ab9c8db9ff673014a0508891a77428bf4683
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-06-06 21:41:29 +0300

    Add other query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,7 +8,7 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-    
+
     # /users
     field :users, [Types::UserType], description: 'Get list of users'
 
@@ -16,12 +16,45 @@ module Types
       User.preload(:posts, posts: :comments)
     end
 
-    field :user, Types::UserType, null: false do
+    # /user
+    field :user, Types::UserType, null: false, description: 'Get user by id' do
       argument :id, ID, required: true
     end
 
     def user(id:)
       User.find(id)
+    end
+
+    # /posts
+    field :posts, [Types::PostType], description: 'Get list of posts'
+
+    def posts
+      Post.preload(:user, :comments)
+    end
+
+    # /post
+    field :post, Types::PostType, null: false, description: 'Get post by id' do
+      argument :id, ID, required: true
+    end
+
+    def post(id:)
+      Post.find(id)
+    end
+
+    # /comments
+    field :comments, [Types::CommentType], description: 'Get list of comments'
+
+    def comments
+      Comment.preload(:user, :post)
+    end
+
+    # /comment
+    field :comment, Types::CommentType, null: false, description: 'Get comment by id' do
+      argument :id, ID, required: true
+    end
+
+    def comment(id:)
+      Comment.find(id)
     end
   end
 end
